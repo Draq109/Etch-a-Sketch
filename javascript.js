55fc0fcec7699fcad3af7018f4bb61c3aadfd981
Draq109 <daniel.vazquez03@utrgv.edu>
2022-10-25 16:09:26 -0500

    Add user interaction with grid and style to header

--- javascript.js
+++ javascript.js
@@ -1,12 +1,22 @@
 const container = document.querySelector('.container');
+const boxes = [], rows = [];
 
 
 for (let i = 0; i < 16; i++) {
     const row = document.createElement('div');
-    row.setAttribute('style', 'display: flex; flex: 1 0 0; width: 32rem; height: auto;')
+    row.setAttribute('style', 'display: flex; flex: 1;  width: 32rem; height: auto;')
+    rows.push(row);
     for (let j = 0; j < 16; j++) {
         const box = document.createElement('div');
-        box.setAttribute('style', 'border: solid black .1rem; flex: 1;')
+        boxes.push(box);
+        box.setAttribute('style', 'background-color: white; flex: 1;')
+        box.addEventListener('mouseover', () => {
+            box.setAttribute('style', 'background-color: navy; flex: 1;')
+        })
+        box.addEventListener('mouseout', () => {
+            box.setAttribute('style', 'background-color: #00FF7F; flex: 1;animation: color-change-bg 3s ')
+        })
+
         row.appendChild(box);
     }
     container.appendChild(row);
